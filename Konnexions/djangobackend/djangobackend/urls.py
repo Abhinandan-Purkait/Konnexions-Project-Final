51c6f64db04daf9f1489ed1923ffe184708e19b5
Abhinandan-Purkait <32007083+Abhinandan-Purkait@users.noreply.github.com>
2018-11-06 20:01:10 +0530

    TRY IT

--- Konnexions/djangobackend/djangobackend/urls.py
+++ Konnexions/djangobackend/djangobackend/urls.py
@@ -6,12 +6,12 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',views.home),
-    url(r'^tictactoe/$',views.tictactoe),
-    url(r'^imagegallery/$',views.imagegallery),
-    url(r'^clickcounter/$',views.clickcounter),
-    url(r'^videogalleryform/$',views.videogalleryform),
-    url(r'^videogallery/$',views.videogallery),
+    url(r'^$',views.homepage,name='home_url_name'),
+    url(r'^tictactoe/$',views.tictactoe,name='tictactoe_url_name'),
+    url(r'^imagegallery/$',views.imagegallery,name='imagegallery_url_name'),
+    url(r'^clickcounter/$',views.clickcounter,name='clickcounter_url_name'),
+    url(r'^videogalleryform/$',views.videogalleryform,name='videogalleryform_url_name'),
+    url(r'^videogallery/$',views.videogallery,name='videogallery_url_name'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
